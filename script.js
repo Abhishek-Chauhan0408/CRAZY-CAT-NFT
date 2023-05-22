6826e4c307f3491c7c6ca831a0ff710712c3b0da
Abhishek-Chauhan0408 <abhishek.skt02@gmail.com>
2023-05-22 17:57:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+
+gsap.to("#card-1",{
+    y:-500,
+    delay:0,
+    opacity:1,
+    scrollTrigger:{
+        // scroller:"#main22",
+        trigger:"#card-1",
+        start:"top 90%",
+        // markers:true,
+        pin:true,
+    //    scrub:1
+    }
+})
+gsap.to("#card-2",{
+    y:-450,
+    delay:0,
+    opacity:1,
+    scrollTrigger:{
+        // scroller:"#main",
+        trigger:"#card-2",
+        start:"top 80%",
+        // markers:true,
+        pin:true,
+    //    scrub:1
+    }
+})
+gsap.to("#card-3",{
+    y:-400,
+    delay:0,
+    opacity:1,
+    scrollTrigger:{
+        // scroller:"#main",
+        trigger:"#card-3",
+        start:"top 70%",
+        // markers:true,
+        pin:true,
+    //    scrub:1
+    }
+})
+gsap.to("#card-4",{
+    y:-350,
+    delay:0,
+    opacity:1,
+    scrollTrigger:{
+        // scroller:"#main",
+        trigger:"#card-4",
+        start:"top 60%",
+        // markers:true,
+        pin:true,
+    //    scrub:1
+    }
+})
+gsap.to("#card-5",{
+    y:-300,
+    delay:0,
+    opacity:1,
+    scrollTrigger:{
+        // scroller:"#main",
+        trigger:"#card-5",
+        start:"top 50%",
+        // markers:true,
+        pin:true,
+    //    scrub:1
+    }
+})
+gsap.to("#card-6",{
+    y:-250,
+    delay:0,
+    opacity:1,
+    scrollTrigger:{
+        // scroller:"#main",
+        trigger:"#card-6",
+        start:"top 40%",
+        // markers:true,
+        pin:true,
+    //    scrub:1
+    }
+})
+gsap.to("#main22",{
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#page12",
+        start:"top 100%",
+        scrub:true
+    }
+})
